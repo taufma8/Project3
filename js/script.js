71310b01472801de22e78438d8f81bfe58cea602
Maliha Taufiq <maliha.taufiq@verizon.com>
2019-06-25 16:46:41 -0500

    added other field to index

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-$('#other').hide();
+//Focuses on the first element on page.
+$('#name').focus();
 
+// $('#other-title').hide();
+$('#title').on('change', function() {
+    if('#title' === 'other') {
+        $('#other-title').show();
+    } else {
+        $('#other-title').hide();
+    }
+});
+
+
+// val().show(otherTitle);
