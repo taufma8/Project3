23040b264e7d11c32b483c16fd5620a8562d975e
Maliha Taufiq <maliha.taufiq@verizon.com>
2019-06-26 16:23:16 -0500

    job role and payment sections

--- js/script.js
+++ js/script.js
@@ -1,14 +1,83 @@
 //Focuses on the first element on page.
 $('#name').focus();
 
-// $('#other-title').hide();
+//Hides the 'Your job role' text box. 
+$('#other-title').hide();
+
+//Text box only appears when 'other' is chosen.
 $('#title').on('change', function() {
-    if('#title' === 'other') {
+    let value = $('#title').val();
+    if(value === 'other') {
         $('#other-title').show();
     } else {
-        $('#other-title').hide();
+        $('#other-title').hide();    
     }
 });
 
+//Hiding the option 'select theme'
+$('#design option:first-child').hide();
 
-// val().show(otherTitle);
+//Updating the color field and hiding the colors in the dropdown menu.
+const optionTheme = $('<option selected>Please select a T-shirt theme</option>');
+$('#color').prepend(optionTheme);
+
+
+//hiding everything in color but the first one.
+$('#color option').hide();
+
+
+//create a change function that if js puns is chosen, then it will populate colors specific to that.
+
+$('#design').on('change', function() {
+    const design = $('#design').val();
+
+    if(design === 'js puns'){
+//show js puns
+        /JS Puns/g.show();
+        // search(/JS Puns/).show();
+//   $('#color option').find('option:contains("JS Puns")').show();
+    } else {
+//show heart js
+        // search(/I &#9829; JS/).show();
+
+    }
+
+});
+
+
+
+// $('.child:not(:first)').hide();
+
+// $(optionTheme).attr('class', 'instruction');
+// $('.instruction').hide();
+
+// const color = $('#color').val();
+// color.hide();
+// $('#color').hide();
+
+// $('').on('change', function() {
+//     if( === '') {
+        
+//     } else {
+          
+//     }
+// });
+
+
+// $('#design').on('change', function() {
+//     let theme = $('#design').val();
+//     theme === 'Select Theme'.hide();
+//     }
+// );
+
+$('#payment option:first-child').hide();
+$('#payment').on('change', function() {
+    const value = $(this).val();
+    if(value == 'credit card') {
+        $('#credit-card').show().next().hide().next().hide();
+    } else if (value == 'paypal') {
+        $('#credit-card').hide().next().show().next().hide();
+    } else {
+        $('#credit-card').hide().next().hide().next().show();
+    }
+});
