dc863ca2c30a7807da30bac53499bd49245b66a0
Maliha Taufiq <mtaufiq02@gmail.com>
2019-07-05 16:40:30 -0500

    almost done.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 //Focuses on the first element on page.
 $('#name').focus();
 
@@ -32,7 +33,6 @@ $('#design').on('change', function() {
     $(`#color option:contains("${matchedString}")`).show();
     $(`#color option:not(:contains("${matchedString}"))`).hide();  
 });
-
 
 //activity section
 const totalCost = '<label></label>';
@@ -70,34 +70,63 @@ $(activities).on('change', function() {
     $('.activities').append('<p id="total-cost"></p>')
 
     $('#total-cost').html('Total Cost: $' + totalActivityCost);
+});
 
-    // $(activities).each(function (index, element) {
-    //     switch(element.name) {
-    //         case "js-frameworkschecked" :
-    //         $('.activities input[name=express]').prop('disabled', true);
-    //         break;
-    //         // case "js-libs" :
-            // $('.activities').prop('disabled', true);
-            // break;
-            // case "express" :
-            // $('.activities').prop('disabled', true);
-            // break;
-            // case "node" :
-            // $('.activities').prop('disabled', true);
-            // break;
-            // case "build-tools" :
-            // $('.activities').prop('disabled', true);
-            // break;
-            // case "npm" :
-            // $('.activities').prop('disabled', true);
-            // break;  
+//Defining variables
+const mainConf = $('input[name="all"]');
+const jsFrameworks = $('input[name="js-frameworks"]');
+const jsLibraries = $('input[name="js-libs"]');
+const express = $('input[name="express"]');
+const nodejs = $('input[name="node"]');
+const build = $('input[name="build-tools"]');
+const npm = $('input[name="npm"]');
 
-    $('.activities').append('<p id="total-cost"></p>')
+//Disabling if activities conflicting times
+$(jsFrameworks).on('change', function() {
+		if ($(this).prop("checked")) {
+			express.prop("disabled", true);
+			express.parent().css("color", "grey");
+		} else {
+            express.prop("disabled", false);
+            express.parent().css("color", "black");
+		}
+    });
 
-    $('#total-cost').html('Total Cost: $' + totalActivityCost);
+$(express).on('change', function() {
+    if ($(this).prop("checked")) {
+        jsFrameworks.prop("disabled", true);
+        jsFrameworks.parent().css("color", "grey");
+    } else {
+        jsFrameworks.prop("disabled", false);
+        jsFrameworks.parent().css("color", "black");
+
+    }
+});
+
+$(jsLibraries).on('change', function() {
+    if ($(this).prop("checked")) {
+        nodejs.prop("disabled", true);
+        nodejs.parent().css("color", "grey");
+    } else {
+        nodejs.prop("disabled", false);
+        nodejs.parent().css("color", "black");
+
+    }
+});
+
+$(nodejs).on('change', function() {
+    if ($(this).prop("checked")) {
+        jsLibraries.prop("disabled", true);
+        jsLibraries.parent().css("color", "grey");
+    } else {
+        jsLibraries.prop("disabled", false);
+        jsLibraries.parent().css("color", "black");
+
+    }
 });
 
 //payment section
+$('fieldset div p').hide();
 $('#payment option:first-child').hide();
 $('#payment').on('change', function() {
     const value = $(this).val();
@@ -105,58 +134,136 @@ $('#payment').on('change', function() {
         $('#credit-card').show().next().hide().next().hide();
     } else if (value == 'paypal') {
         $('#credit-card').hide().next().show().next().hide();
+        $('fieldset div p').show();
     } else {
         $('#credit-card').hide().next().hide().next().show();
+        $('fieldset div p').show();
     }
 });
 
-    // const label = $(checkboxInput).parent().text();
-    //$('.activities label').text();
-    // console.log(checkboxInput);
-    // regEx = /^[$]/;
-    // const $ = activities.match(regEx);
-    // const regExActivities = /(.*)\s—\s((.*)\s\d(.*)-\d(.*),\s)?[$]\d+/;
-    // const regExTitle = /(.*)\s—/;
-    // const regExSchedule = /\s—\s(.*)\s\d(.*)-\d(.*),/
-    // const regExCost = /[$]\d+/;
-//     const string = "Main Conference — $200";
-//     const indexOfVar = "$"
-// $(string).indexOf("$");
 
-    // const mainConf = 200;
-    // const jsFramework = 100;
-    // const jsLibraries = 100;
-    // const express = 100;
-    // const nodejs = 100;
-    // const build = 100;
-    // const npm = 100;
+//validation section
+function validateName() {
+    const nameRegEx = /^[a-zA-Z]+$/;
+    const name = $('#name').val();
+    let nameNotValid = false;
 
+    if (!nameRegEx.test(name) || name == '') {
+        $('input[name="user_name"]').css("border-color", "red");
+        $('#name').prev().css("color", "red");
+        nameNotValid = true;
+    } else {
+        $('input[name="user_name"]').css("border-color", "#b0d3e2");
+        $('#name').prev().css("color", "black");
+    }
+}
 
-    // const mainConf = $('.activities input[name="all"]);
-// const jsFramework = 100;
-// const jsLibraries = 100;
-// const express = 100;
-// const nodejs = 100;
-// const build = 100;
-// const npm = 100;
+function validateEmail() {
+    const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const email = $('#mail').val();
+    let emailNotValid = false;
 
-// $(activities) = {
-//     name: "all",
-//     value: "200"
-// },
-// {
-//     name: "jsFramework",
-//     value: "100"
-// };
+    if (!emailRegEx.test(email) || email == '') {
+        $('input[name="user_email"]').css("border-color", "red");
+        $('#mail').prev().css("color", "red");
+        emailNotValid = true;
+    } else {
+        $('input[name="user_email"]').css("border-color", "#b0d3e2");
+        $('#mail').prev().css("color", "black");
+    }
+}
 
+function validateActivities() {
+    let numberOfActivities = [];
+    $('.activities input:checked').each(() => {
+        numberOfActivities.push($(this).text());
+    });
+    activitiesNotValid = false;
 
-    // if (checkboxInput == 'input[name=all]') {
-    //     totalActivityCost += 200;
-    // } else if (checkboxInput == regExTitle) {
-    //     totalActivityCost += 100;
-    // } else if (checkboxInput == regExTitle) {
-    //     totalActivityCost += 100;
-    // } else {
-    //     totalActivityCost += 100;
-    // }  console.log(totalActivityCost);
-    // });
+    if (numberOfActivities.length < 1) {
+        $('.activities legend').css("color", "red");
+        activitiesNotValid = true;
+    } else {
+        $('.activities legend').css("color", "rgba(6, 49, 68, 0.9)");
+    }
+}
+
+function validateCard() {
+    const creditRegEx = /^[0-9]{13,16}?$/;
+    const credit = $("#cc-num").val();
+    let ccNotValid = false;
+
+    if (!creditRegEx.test(credit) || credit == '') {
+        $('input[name="user_cc-num"]').css("border-color", "red");
+        $('#cc-num').prev().css("color", "red");
+        ccNotValid = true;
+    } else {
+        $('input[name="user_cc-num"]').css("border-color", "#b0d3e2");
+        $('#cc-num').prev().css("color", "black");
+
+    }
+}
+
+function validateZip() {
+    const zipRegEx = /^[0-9]{5}$/;
+    const zip = $("#zip").val();
+
+    if (!zipRegEx.test(zip) || zip == ''){
+        $('input[name="user_zip"]').css("border-color", "red");
+        $('#zip').prev().css("color", "red");
+        ccNotValid = true;
+
+    } else {
+        $('input[name="user_zip"]').css("border-color", "#b0d3e2");
+        $('#zip').prev().css("color", "black");
+
+    }
+}
+
+function validateCvv() {
+    const cvvRegEx = /^[0-9]{3}$/;
+    const cvv = $("#cvv").val();
+
+    if (!cvvRegEx.test(cvv) || cvv == ''){
+        $('input[name="user_cvv"]').css("border-color", "red");
+        $('#cvv').prev().css("color", "red");
+        ccNotValid = true;
+
+    } else {
+        $('input[name="user_cvv"]').css("border-color", "#b0d3e2");
+        $('#cvv').prev().css("color", "black");
+
+    }
+}
+function checkCreditCard(){
+    if (payment === "credit card"){
+        validateCard();
+        validateZip();
+        validateCvv();
+    } 
+}
+
+$(document).ready(function () {
+
+    $('button:submit').on('click', function(e) {
+        e.preventDefault();
+        validateName();
+        validateEmail();
+        validateActivities();
+        checkCreditCard();
+
+        nameNotValid = true;
+        emailNotValid = true;
+        activitiesNotValid = true; 
+        ccNotValid = true;
+
+        if (nameNotValid || emailNotValid || activitiesNotValid || ccNotValid){
+            alert("Registration unsuccessul. Please correct the inputs marked red.")
+        } 
+        else{
+            alert("Registration successful! See you at the conference!")
+            location.reload(true);
+        }  
+
+    }); 
+}); 
